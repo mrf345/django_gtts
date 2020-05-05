e0fd444a66e715a3daff0095cb8a88fda17c5c20
Mohamed Feddad <mrf345@gmail.com>
2020-05-05 11:51:04 +0300

    Fix setup pypi markdown description. And bump version to 0.3

--- setup.py
+++ setup.py
@@ -4,20 +4,27 @@ Django-Gtts
 A Django app to add gTTS google text-to-speech to the template 
 with ability to cache text-to-speech .mp3 files generated.
 """
+from os import path
 from setuptools import setup, find_packages
+
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'),
+          encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
     name='Django-Gtts',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     url='https://github.com/mrf345/django_gtts/',
-    download_url='https://github.com/mrf345/django_gtts/archive/0.2.tar.gz',
+    download_url='https://github.com/mrf345/django_gtts/archive/0.3.tar.gz',
     license='MIT',
     author='Mohamed Feddad',
     author_email='mrf345@gmail.com',
     description='gTTS google text-to-speech django app',
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     platforms='any',
     install_requires=[
